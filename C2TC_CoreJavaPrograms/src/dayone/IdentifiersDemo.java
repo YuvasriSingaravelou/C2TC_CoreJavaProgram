1bb78e1437f194398bd2891b9ba1f0b29b6a1d75
Yuvasri <160617670+YuvasriSingaravelou@users.noreply.github.com>
2025-07-22 09:34:19 +0530

    This is my basic java programs

--- C2TC_CoreJavaPrograms/src/dayone/IdentifiersDemo.java
+++ C2TC_CoreJavaPrograms/src/dayone/IdentifiersDemo.java
@@ -1,5 +1,15 @@
 package dayone;
 
 public class IdentifiersDemo {
+	public static void main(String[] args) {
+		
+		 
+		
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName);
+	}
 
 }
--- C2TC_CoreJavaPrograms/src/daytwo/ForLoopExample.java
+++ C2TC_CoreJavaPrograms/src/daytwo/ForLoopExample.java
@@ -1,5 +1,12 @@
 package daytwo;
 
 public class ForLoopExample {
-
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=1000 ; i++)   
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
 }
